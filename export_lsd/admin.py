9bb0929ab2fcabb1b44cf4d0789e3c3ce5c7ca65
Eugenio <lugezz@gmail.com>
2023-02-21 12:08:48 -0300

    Admin View - Empresa y Empleado

--- export_lsd/admin.py
+++ export_lsd/admin.py
@@ -7,10 +7,22 @@ from export_lsd.models import (BasicExportConfig, ConceptoLiquidacion, Empleado,
 
 admin.site.register(BasicExportConfig)
 admin.site.register(ConceptoLiquidacion)
-admin.site.register(Empleado)
-admin.site.register(Empresa)
 admin.site.register(Formato931)
 admin.site.register(Liquidacion)
 admin.site.register(OrdenRegistro)
 admin.site.register(Presentacion)
 admin.site.register(TipoRegistro)
+
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ("leg", "name", "empresa", "cuil", "area")
+    list_filter = ("empresa", "area")
+    list_per_page = 30
+
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ("name", "cuit", "user")
+    list_filter = ("user", )
+    list_per_page = 30
